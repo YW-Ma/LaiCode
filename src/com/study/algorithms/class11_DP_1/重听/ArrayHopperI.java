07d5c8a9ae398a739ae33f67769e3230ea068c64
麻尧崴 <ma_yaowei@outlook.com>
2021-06-17 21:40:00 +0800

    feat: array hopper i

--- src/com/study/algorithms/class11_DP_1/重听/ArrayHopperI.java
+++ src/com/study/algorithms/class11_DP_1/重听/ArrayHopperI.java
@@ -21,6 +21,10 @@ public class ArrayHopperI {
     //          ^
     //             超出范围，停下来
 
+    // [1,0,0,100,1]
+    //  T T F F   F
+    //        ^ 这里是F，就算可以跳100步，也因为走不到这里，而没有用。
+
     // base rule: M[0] = T
     // induction rule: M[i] = true,  if there exists a j where j < i and (M[j] == true && A[j] >= i)
     //                        false, otherwise.
@@ -28,6 +32,24 @@ public class ArrayHopperI {
     // code其实就是for循环i一个一个检查能否跳到结尾。因为，如上图，下一次尝试的，肯定是下一个index。
     // Time: O(n^2)
     // Space: O(n)
+    public boolean canJump1(int[] array) {
+        boolean[] can = new boolean[array.length]; // can[i] --> from stat can jump to i or not
+        // base rule:
+        can[0] = true;
+        // induction rule:
+        for (int i = 0; i < array.length; i++) {
+            if (!can[i]) {                                  // 【1】当前是true，才能跳，否则找下一个true的
+                continue;
+            }
+            for (int j = 1; j <= array[i]; j++) {
+                if (i + j >= array.length) {
+                    return true;
+                }
+                can[i + j] = true;
+            }
+        }
+        return can[array.length - 1];
+    }
 
 
     // Solution 2 （推荐：原因是可以进一步求最小步数）
@@ -47,5 +69,19 @@ public class ArrayHopperI {
     // code其实就是从后往前一个一个尝试
     // 找之前可达我这里的点
     // 最后看看开头是不是可达我这里的点。
-
+    public boolean canJump2(int[] array) {
+        boolean[] can = new boolean[array.length]; // can[i] --> can jump to end or not
+        // base rule:
+        can[array.length - 1] = true;
+        // induction rule:
+        for (int i = array.length - 2; i >= 0; i--) {
+            for (int j = array[i]; j >= 1; j--) {
+                if ((i + j >= array.length) || can[i + j]) {    // 从我这里开始往后摸，摸到一个true的，就说明我也可以跳到结尾了。
+                    can[i] = true;
+                    break;
+                }
+            }
+        }
+        return can[0]; // 返回开头能不能到结尾
+    }
 }
