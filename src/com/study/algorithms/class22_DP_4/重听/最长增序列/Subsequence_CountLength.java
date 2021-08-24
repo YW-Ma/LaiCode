27799c2636591caffe3b13c956bbe51cc1ef61ac
Yaowei_Ma <mayaowei.study@gmail.com>
2021-08-24 16:07:59 -0700

    subsequence

--- src/com/study/algorithms/class22_DP_4/重听/最长增序列/Subsequence_CountLength.java
+++ src/com/study/algorithms/class22_DP_4/重听/最长增序列/Subsequence_CountLength.java
@@ -1,55 +1,28 @@
 package com.study.algorithms.class22_DP_4.重听.最长增序列;
 
 public class Subsequence_CountLength {
-    public int longest_after_review(int[] array) {
-        // review的修改：
-        // 1. 可以沈略prev，prev的比较逻辑是可以和i的更新逻辑合并的。
-        // 2. 默认值可以放在最前面 (DP[i] = 1)
+    // review: 默认值可以先写。 但是那样要让globalMax也从默认值开始。
+    // 一个月后重新做的：感觉写的最好。
+    // O(n^2)
+    public int longest(int[] array) {
         if (array == null || array.length == 0) {
             return 0;
         }
-        
-        int[] DP = new int[array.length];
+
+        int[] length = new int[array.length]; // ith ele record the LAS ends at i
+        length[0] = 1;
         int globalMax = 1;
-        DP[0] = 1;
-        
         for (int i = 1; i < array.length; i++) {
-            DP[i] = 1;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] < array[i] && DP[j] + 1 > DP[i]) {
-                    DP[i] = DP[j] + 1;
-                    globalMax = Math.max(globalMax, DP[i]);
+            int curMax = 0;
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[i]) {
+                    curMax = Math.max(curMax, length[j]);
                 }
             }
+            length[i] = curMax + 1;
+            globalMax = Math.max(globalMax, length[i]);
         }
-        return globalMax;
-    }
-    
-    public int longest_before_review(int[] array) {
-        // 最基本的算法：look back to previous i-1 elements to find out the one to add after。 所以是O(n) * O(n) * O(1) = O(n^2)
-        if (array == null || array.length == 0) {
-            return 0;
-        }
-        
-        int[] DP = new int[array.length];
-        int globalMax = 1;
-        DP[0] = 1;
-        
-        for (int i = 1; i < array.length; i++) {
-            int prev = i;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] < array[i] && DP[j] > DP[prev]) {
-                    prev = j;
-                }
-            }
-            
-            if (prev != i) {
-                DP[i] = DP[prev] + 1;
-                globalMax = Math.max(globalMax, DP[i]);
-            } else {
-                DP[i] = 1;
-            }
-        }
+
         return globalMax;
     }
 }
--- src/com/study/algorithms/class22_DP_4/重听/最长增序列/Subsequence_CountLength_BinarySearchSolution.java
+++ src/com/study/algorithms/class22_DP_4/重听/最长增序列/Subsequence_CountLength_BinarySearchSolution.java
@@ -1,4 +1,5 @@
 package com.study.algorithms.class22_DP_4.重听.最长增序列;
 
 public class Subsequence_CountLength_BinarySearchSolution {
+
 }
