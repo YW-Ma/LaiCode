4d8aadf3b7fde5573a265b3303d7a15160ac53a0
Yaowei_Ma <mayaowei.study@gmail.com>
2021-08-24 17:52:44 -0700

    subsequence get sequence

--- src/com/study/algorithms/class22_DP_4/重听/最长增序列/Subsequence_GetSubsequence_S1.java
+++ src/com/study/algorithms/class22_DP_4/重听/最长增序列/Subsequence_GetSubsequence_S1.java
@@ -3,48 +3,48 @@ package com.study.algorithms.class22_DP_4.重听.最长增序列;
 import java.util.*;
 
 public class Subsequence_GetSubsequence_S1 {
-    // 现在希望不只是统计最长的长度，还希望知道路径
+
+    // review: globalMax 可以被 globalMaxIndex代替，每次需要用globalMax的时候去访问length即可。 所以只需要一个变量，而不需要两个。
     public int[] longest(int[] array) {
-        // 最基本的算法：look back to previous i-1 elements to find out the one to add after。 所以是O(n) * O(n) * O(1) = O(n^2)
         if (array == null || array.length == 0) {
             return new int[0];
         }
-    
-        int[] DP = new int[array.length];
-        int[] Prev = new int[array.length];
-        int globalMaxIndex = 0; // 要从0开始，不能从1开始。 不确定是否有2个
-        DP[0] = 1;
-        Prev[0] = 0; // Assume the prev of first element is itself.
-    
-        for (int i = 1; i < array.length; i++) {
-            DP[i] = 1;
-            Prev[i] = i;
-            for (int j = 0; j < i; j++) { // 一定只能在前面找，才符合逻辑
-                if (array[j] < array[i] && DP[j] + 1 > DP[i]) {
-                    DP[i] = DP[j] + 1;
-                    Prev[i] = j;
-                    if (DP[globalMaxIndex] < DP[i]) {
-                        globalMaxIndex = i;
+
+        int[] length = new int[array.length];
+        int[] prev = new int[array.length];
+        int globalMaxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            int curMax = 0;
+            prev[i] = i; // review：这样初始化才行。
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[i]) {
+                    if (curMax <= length[j]) {
+                        curMax = length[j];
+                        prev[i] = j;
                     }
+                }
+
+                length[i] = curMax + 1;
+                if (length[globalMaxIndex] <= length[i]) { // 这里能决定，相等时是取前面的还是后面的。这里选择是取后面，下面的方法是选择取后面。
+                    globalMaxIndex = i;
                 }
             }
         }
-        
-        // 从globalMaxIndex往回撸一遍，就知道顺序了
-        List<Integer> result = new ArrayList<>();
+        Queue<Integer> container = new ArrayDeque<>();
         int curIndex = globalMaxIndex;
-        int prevIndex = Prev[globalMaxIndex];
-        // while (curIndex != prevIndex) {  // 这个循环的终止条件，会导致 == 的情况被漏掉。 要记得补上 【2】   （切勿把 == 的情况放到while里，会死循环）
-        while (curIndex != prevIndex) {
-            result.add(array[curIndex]);
+        int prevIndex = prev[curIndex];
+        while (curIndex != prevIndex) { // 条件是prev和自己不相同。
+            container.offer(array[curIndex]);
             curIndex = prevIndex;
-            prevIndex = Prev[curIndex];
+            prevIndex = prev[curIndex];
         }
-        result.add(array[curIndex]);
-        
-        int[] res = new int[result.size()];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = result.get(res.length - 1 - i); // 注意，result里的顺序是从后向前的，这里要反过来一下。   【1】
+        // 出来的时候curIndex == 0
+        container.add(array[curIndex]);
+
+        // 存储到int[]里面
+        int[] res = new int[container.size()];
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] = container.poll();
         }
         return res;
     }
---
+++ src/com/study/algorithms/class22_DP_4/重听/最长增序列/Tester.java
@@ -0,0 +1,11 @@
+package com.study.algorithms.class22_DP_4.重听.最长增序列;
+
+import java.util.Arrays;
+
+public class Tester {
+    public static void main(String[] args) {
+        Subsequence_GetSubsequence_S1 s1 = new Subsequence_GetSubsequence_S1();
+        int[] res = s1.longest(new int[]{5, 2, 6, 3, 4, 7, 5});
+        System.out.println(Arrays.toString(res));
+    }
+}
