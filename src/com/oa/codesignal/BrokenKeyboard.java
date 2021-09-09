bc534553529160f6ba9befdaccca4aa414dc24c6
Yaowei_Ma <mayaowei.study@gmail.com>
2021-09-08 23:57:19 -0700

    Broken Keyboard

--- src/com/oa/codesignal/BrokenKeyboard.java
+++ src/com/oa/codesignal/BrokenKeyboard.java
@@ -1,5 +1,9 @@
 package com.oa.codesignal;
 
+// 题目是键盘坏了，只剩下一组valid letters中的字母按键和所有的数字和符号案件能用，
+// 同时shift键是好的， 所以可以切换大小写。
+// 问一组words中的单词有几个可以用当前坏掉的键盘打出来
+
 // 输入一组words和一组valid letters，判断有多少个words是valid。
 // 判断条件是words里的所有 upper and lower letter必须在valid letters里面。
 // 如果word里面有special character不用管。
@@ -10,6 +14,35 @@ package com.oa.codesignal;
 // "hello##" 就是valid，因为h，e，l，o都在valid letter 里面，
 // “This^^” 不valid, 因为i不在valid letter里面
 
+import java.util.HashSet;
+
 // input: a = "Hello, my dear friend!", b = ['h', 'e', 'l', 'o', 'm']
 public class BrokenKeyboard {
+    public int brokenKeyBoard(String str, char[] letters) {
+        // preprocess str into word array
+        String[] array = str.split(" "); // 按照空格分开
+
+        // preprocess letters into HashSet
+        boolean[] letterSet = new boolean[256];
+        for (char ch : letters) {
+            letterSet[ch] = true;
+        }
+
+        // check each string one by one
+        int counter = 0;
+        for (String word : array) {
+            word = word.toLowerCase();
+            boolean isValid = true;
+            for (char ch : word.toCharArray()) {
+                if (Character.isLetter(ch) && !letterSet[ch]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
--- src/com/oa/codesignal/Tester.java
+++ src/com/oa/codesignal/Tester.java
@@ -5,5 +5,9 @@ public class Tester {
         CompareStringWithFrequency compare = new CompareStringWithFrequency();
         boolean res = compare.compareStrings("babzccc", "abbzczz");
         System.out.println(res);
+
+        BrokenKeyboard brokenKeyboard = new BrokenKeyboard();
+        int counter = brokenKeyboard.brokenKeyBoard("world! Hello, world! hee oo", new char[]{'h','e','l','o'});
+        System.out.println(counter);
     }
 }
