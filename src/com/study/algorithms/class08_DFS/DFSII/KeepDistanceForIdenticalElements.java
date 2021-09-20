82816e1d2df4c729bb124ac93c5c6a72b7a03764
Yaowei_Ma <mayaowei.study@gmail.com>
2021-09-20 16:10:44 -0700

    KeepDistanceForIdenticalElements

--- src/com/study/algorithms/class08_DFS/DFSII/KeepDistanceForIdenticalElements.java
+++ src/com/study/algorithms/class08_DFS/DFSII/KeepDistanceForIdenticalElements.java
@@ -1,4 +1,40 @@
 package com.study.algorithms.class08_DFS.DFSII;
 
+import java.util.Arrays;
+
 public class KeepDistanceForIdenticalElements {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(keepDistance(1)));
+    }
+    
+    public static int[] keepDistance(int k) {
+        // Write your solution here.
+        int[] pad = new int[2 * k];
+        return helper(pad, k) ? pad : null;
+    }
+    
+    private static boolean helper(int[] pad, int num) {
+        if (num == 0) {
+            return true;
+        }
+        
+        int left = 0;
+        int right = left + num + 1;
+        while (right < pad.length) {
+            if (pad[left] == 0 && pad[right] == 0) {
+                pad[left] = num;
+                pad[right] = num;
+                
+                if (helper(pad, num - 1) == true) { // 找到了一个解
+                    return true;
+                }
+                
+                pad[left] = 0;
+                pad[right] = 0;
+            }
+            left++;
+            right = left + num + 1;
+        }
+        return false; // 当前层根本找不到，就返回false，让换一个上层元素的位置。
+    }
 }
--- src/com/study/algorithms/class08_DFS/DFSII/Tester.java
+++ src/com/study/algorithms/class08_DFS/DFSII/Tester.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5);
-        list.subList(2,4).clear();
     }
 }
 // abbb
