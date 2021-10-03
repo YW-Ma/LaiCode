09eddf07371fb3aecb043045c4e12829172c7d44
Yaowei_Ma <mayaowei.study@gmail.com>
2021-10-03 14:24:21 -0700

    is sub-matrix full

--- src/com/oa/codesignal/isSubmatrixFull.java
+++ src/com/oa/codesignal/isSubmatrixFull.java
@@ -1,65 +1,60 @@
 package com.oa.codesignal;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class IsSubmatrixFull {
     public static boolean[] isSubmatrixFull_Ref(int[][] numbers) {
         int[] num = new int[10];
         int row = 3;
         int col = numbers[0].length;
-        boolean[] res = new boolean[col-2];
+        boolean[] res = new boolean[col - 2];
         int count = 0;
-        for(int i=0; i<3; ++i){
-            for(int j=0; j<3; ++j){
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
                 int tmp = numbers[i][j];
-                if(num[tmp] == 0){
+                if (num[tmp] == 0) {
                     count++;
                 }
                 num[tmp]++; // num 似乎是记录每个value出现的次数的。
             }
         }
-        res[0] = false;
-        if(count == 9)
-            res[0] = true;
-
-
-        for(int i=1; i<col-2; ++i){
+        res[0] = count == 9;
+        
+        
+        for (int i = 1; i < col - 2; ++i) {
             int tmp = 0;
-            for(int j=0; j<3; ++j){ // tmp 用来拉取某个九宫格window左侧←的三个element
-                tmp = numbers[j][i-1];
+            for (int j = 0; j < 3; ++j) { // tmp 用来拉取某个九宫格window左侧←的三个element
+                tmp = numbers[j][i - 1];
                 num[tmp]--; // 减去这个值
-                if(num[tmp] == 0){
+                if (num[tmp] == 0) {
                     count--; // 对应的count--
                 }
             }
-
-            for(int j=0; j<3; ++j){ // tmp 用来拉取某个九宫格window中，最右侧→的element
-                tmp = numbers[j][i+2];
-                if(num[tmp] == 0){
+            
+            for (int j = 0; j < 3; ++j) { // tmp 用来拉取某个九宫格window中，最右侧→的element
+                tmp = numbers[j][i + 2];
+                if (num[tmp] == 0) {
                     count++; // 相当于把剪掉的那个加回来。
                 }
                 num[tmp]++;
             }
             // 其实原理很简单，把window移动后，把左边移出去的的影像去除，然后添加新的一列。检查是否是9
-
-            if(count == 9)
-                res[i] = true;
-            else
-                res[i] = false;
+    
+            res[i] = count == 9;
         }
         return res;
     }
     
     public static void main(String[] args) {
-        boolean[] res = isSubmatrixFull(new int[][]{{1,2,3,1,1}, {4,5,6,4,4}, {7,8,9,7,7}});
+        boolean[] res = isSubmatrixFull(new int[][]{{1, 2, 3, 1, 1}, {4, 5, 6, 4, 4}, {7, 8, 9, 7, 7}});
         System.out.println(Arrays.toString(res));
     }
-    
-    
+
+
 // input: 3xn int array
 // output: 1x(n-2) boolean array
-
-// get three col -> check
+    
+    // get three col -> check
 // remove the left-most col, add the right-most col. check again
 // use an int array to record the number of 1-9 elements in a 3x3 window
     // array[1] = 3 means, there are three 1 in 3x3 window
@@ -83,11 +78,7 @@ public class IsSubmatrixFull {
                 nums[n]++;
             }
         }
-        if (counter == 9) {
-            res[0] = true;
-        } else {
-            res[0] = false;
-        }
+        res[0] = counter == 9;
         
         for (int i = 1; i < matrix[0].length - 2; i++) {
             // remove left-most col
@@ -109,11 +100,7 @@ public class IsSubmatrixFull {
             }
             
             // check counter
-            if (counter == 9) {
-                res[i] = true;
-            } else {
-                res[i] = false;
-            }
+            res[i] = counter == 9;
         }
         
         return res;
