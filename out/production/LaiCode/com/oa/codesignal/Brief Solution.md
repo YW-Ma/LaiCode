be3e65a3d2c17e083c2a7265325e68256e225057
Yaowei_Ma <mayaowei.study@gmail.com>
2021-10-03 15:14:27 -0700

    split and swap in pairs

--- out/production/LaiCode/com/oa/codesignal/Brief Solution.md
+++ out/production/LaiCode/com/oa/codesignal/Brief Solution.md
@@ -7,7 +7,7 @@
 1. [check operation](./CheckOperation.java) 四个数组，验证第一个数组的数组+第二个数组的符号+第三个数组的数字，是否等于第四个数组的target
 2. [is sub-matrix full](./isSubmatrixFull.java) 注意出一组、入一组
 3. [letter combinations of a phone number](./LetterCombinationsOfPhoneNumber.java) DFS
-4. [strings head and tail](./CheckStringHeadAndTail.java) 返回boolean array
+4. [strings head and tail](./CheckStringHeadAndTail.java) 返回boolean array, 如果相邻的两个string的头和尾相同则return true。otherwise false
 5. [Split and Swap](./SplitAndSwap.java)Given a string and a list of numbers, split the string according to the list and swap it. Return the swapped string.
 6. [Compute Product and Sum](./ComputeProductAndSum.java)  Compute product - sum of a number.
    For example, given 123456, product would be 1x2x3x4x56 = 720, sum would be 1+2+3+4+5+6 = 21, return product - sum
--- src/com/oa/codesignal/SplitAndSwap.java
+++ src/com/oa/codesignal/SplitAndSwap.java
@@ -2,6 +2,39 @@ package com.oa.codesignal;
 
 public class SplitAndSwap {
     public static void main(String[] args) {
+        System.out.println(splitAndSwap("descognail", new int[]{3,2,3,1,1}));
+    }
     
+    public static String splitAndSwap(String str, int[] seg) {
+        // input the str and the length of each segments.
+        // output the new str with each pair of seg swapped.
+        char[] array = str.toCharArray();
+        int segIndex = 0;
+        int curIndex = 0;
+        StringBuilder res = new StringBuilder();
+        
+        while (segIndex < seg.length) {
+            StringBuilder part1 = new StringBuilder();
+            StringBuilder part2 = new StringBuilder();
+            
+            if (segIndex < seg.length && segIndex % 2 == 0) {
+                for (int i = 0; i < seg[segIndex]; i++) {
+                    part1.append(array[curIndex + i]);
+                }
+                curIndex += seg[segIndex];
+                segIndex++;
+            }
+            
+            if (segIndex < seg.length && segIndex % 2 == 1) {
+                for (int i = 0; i < seg[segIndex]; i++) {
+                    part2.append(array[curIndex + i]);
+                }
+                curIndex += seg[segIndex];
+                segIndex++;
+            }
+            
+            res.append(part2.toString()).append(part1.toString());
+        }
+        return res.toString();
     }
 }
