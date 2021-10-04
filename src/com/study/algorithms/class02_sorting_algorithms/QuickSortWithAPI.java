32d9979335634299a8e96b9f52d03b8e7a16fcae
Yaowei_Ma <mayaowei.study@gmail.com>
2021-10-03 18:12:40 -0700

    new

--- src/com/study/algorithms/class02_sorting_algorithms/QuickSortWithAPI.java
+++ src/com/study/algorithms/class02_sorting_algorithms/QuickSortWithAPI.java
@@ -28,7 +28,6 @@ public class QuickSortWithAPI {
                 if (o1.equals(o2)) {
                     return 0;
                 } else if (o1 > o2) {
-                    \]
                     return -1;
                 }
                 return 1;
