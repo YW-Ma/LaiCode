4bf2bb27dd5a09f73cf9da3e9fe989c6fe889fb4
Yaowei_Ma <mayaowei.study@gmail.com>
2022-01-05 14:58:08 -0800

    DicesSum

--- src/com/jiuzhang/BFS进阶与动态规划/邮局的建立II/BFS.java
+++ src/com/jiuzhang/BFS进阶与动态规划/邮局的建立II/BFS.java
@@ -1,4 +1,12 @@
 package com.jiuzhang.BFS进阶与动态规划.邮局的建立II;
 
 public class BFS {
+    final int EMPTY = 0;
+    final int HOUSE = 1;
+    final int WALL = 2;
+
+    // 
+    public int shortestDistance(int[][] grid) {
+        // write your code here
+    }
 }
---
+++ src/com/jiuzhang/概率型动态规划/DicesSum.java
@@ -0,0 +1,32 @@
+package com.jiuzhang.概率型动态规划;
+
+import java.util.*;
+
+public class DicesSum {
+    public List<Map.Entry<Integer, Double>> dicesSum(int n) {
+        double[][] dp = new double[n + 1][6 * n + 1];
+
+        // base rule:
+        for (int j = 1; j <= 6; j++) {
+            dp[1][j] = 1 / 6.0;
+        }
+
+        // induction rule:
+        for (int i = 2; i <= n; i++) { // 枚举骰子的个数
+            for (int j = 1; j <= 6 * n; j++) { // 枚举当前的sum值
+                for (int k = 1; k <= 6; k++) { // 枚举当前骰子的点数
+                    if (j < k) { // 试图寻找上一个状态，如果sum值低于点数，说明没有合适的上一个状态
+                        continue;
+                    }
+                    dp[i][j] += dp[i - 1][j - k] / 6; // 这种k的情况发生的概率是 1/6
+                }
+            }
+        }
+
+        List<Map.Entry<Integer, Double>> ans = new ArrayList<>();
+        for (int j = n; j <= 6 * n; j++) {
+            ans.add(new AbstractMap.SimpleEntry<Integer, Double>(j, dp[n][j]));
+        }
+        return ans;
+    }
+}
